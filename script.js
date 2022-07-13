7012c409c50e1d718852316bb32b1bac7d19f768
Oleksandr <oshengera@gmail.com>
2022-07-13 22:12:41 +0300

    practice_2 add two operators for and if

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const movieName1 = prompt('Один из последних просмотренных фильмов?', '');
-const movieRating1 = prompt('На сколько оцените его?', '');
-const movieName2 = prompt('Один из последних просмотренных фильмов?', '');
-const movieRating2 = prompt('На сколько оцените его?', '');
+first: for (let i = 0; i < 2; i++) {
+        const movieName = prompt('Один из последних просмотренных фильмов?', '');
+        
+        if (movieName != null && movieName != '' && movieName.length < 50) {
+            const movieRating = prompt('На сколько оцените его?', '');
+            if (movieRating != null && movieRating != '') { 
+                personalMovieDB.movies[movieName] = movieRating;
+            } else {
+                i--;
+            }
+        } else {
+            i--;
+        }
+}
 
-personalMovieDB.movies[movieName1] = movieRating1;
-personalMovieDB.movies[movieName2] = movieRating2;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено достаточно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+}  else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+}   else {
+    console.log('Возникла ошибка');
+}
 
 console.log(personalMovieDB);
